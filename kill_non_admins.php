0e651acba06c335c7f9754fb015f0f0146546c86
Tom Woodward <twwoodward@me.com>
2016-02-27 20:56:51 -0500

    using array_intersect instead
    alternate way to do the filtering - seems to be much better and you could tweak it much more easily

--- kill_non_admins.php
+++ kill_non_admins.php
@@ -1,33 +1,30 @@
 <?php
 /*
-Plugin Name: ALT Lab Remove Participant Blogs
+Plugin Name: ALT Lab Remove Ugly Blogs
 Plugin URI: https://github.com/woodwardtw/alt-lab-participant-remover
 Description: Removes all the blogs where you are just a participant
 Author: Tom Woodward
-Version: 1.1
+Version: 1.5
 Author URI: http://bionicteaching.com/
 */
 
-function remove_non_admin_blogs($blogs) {
+function remove_junk_blogs($blogs) {
  				global $current_user; 
     			$user_id = $current_user->ID; 
-    			$role = 'bbp_participant';
+                $allowed_roles = array( 'administrator', 'editor', 'author', 'contributor','subscriber'); 
 
                 foreach ( $blogs as $blog_id => $blog ) {
 
                     // Get the user object for the user for this blog.
                     $user = new WP_User( $user_id, '', $blog_id );
-				if (count( $user->roles ) === 1 ){
 
-
-                    // Remove this blog from the list if the user doesn't have the role for it.
-                    if (in_array( $role, $user->roles ) ) {
-                        unset( $blogs[ $blog_id ] );
+                    if (array_intersect($allowed_roles, $user->roles) ) {
                     }
-                }
+                    else
+                      unset($blogs[$blog_id]);
             }
 
                 return $blogs;
             }    
-add_filter( 'get_blogs_of_user', 'remove_non_admin_blogs' );
+add_filter( 'get_blogs_of_user', 'remove_junk_blogs' );
 ?>
